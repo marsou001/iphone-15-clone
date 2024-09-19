5cb3d47ce5231c9e70381858040eb305da0c7616
marouane <marsou431@gmail.com>
2024-09-19 19:54:45 +0100

    create hero call to action

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,15 +2,22 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { heroVideoPath, smallHeroVideoPath } from "@/constants/paths";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Hero() {
   const [videoSrc, setVideoSrc] = useState("");
 
   useGSAP(() => {
-    gsap.to("p", {
+    gsap.to("p[class='hero-title']", {
       opacity: 1,
       delay: 1.5,
-    })
+    });
+
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2.5,
+    });
   }, []);
 
   function handleVideoSrc() {
@@ -37,6 +44,11 @@ export default function Hero() {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <Link href="#highlights" className="btn">Buy</Link>
+        <p className="font-normal text-xl">from $199/month or $999</p>
       </div>
     </section>
   )
