25efbbeb8db91dda804751e1526166bbc7f02567
marouane <marsou431@gmail.com>
2024-09-19 15:35:20 +0100

    import assets' paths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { navLists } from "@/constants";
+import { appleImgPath, bagImgPath, searchImgPath } from "@/constants/paths";
+import { navLists } from "@/constants/texts";
 
 export default function Navbar() {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src="/assets/images/apple.svg" alt="Apple" width={14} height={18} />
+        <img src={appleImgPath} alt="Apple" width={14} height={18} />
         
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
@@ -13,8 +14,8 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src="/assets/images/search.svg" alt="search" width={18} height={18} />
-          <img src="/assets/images/bag.svg" alt="bag" width={18} height={18} />
+          <img src={searchImgPath} alt="search" width={18} height={18} />
+          <img src={bagImgPath} alt="bag" width={18} height={18} />
         </div>
       </nav>
     </header>
---
+++ src/constants/paths.ts
@@ -0,0 +1,27 @@
+export const heroVideoPath = "/assets/videos/hero.mp4";
+export const smallHeroVideoPath = "/assets/videos/smallHero.mp4";
+export const highlightFirstVideoPath = "/assets/videos/highlight-first.mp4";
+export const highlightSecondVideoPath = "/assets/videos/highlight-firssec.mp4";
+export const highlightThirdVideoPath = "/assets/videos/highlight-third.mp4";
+export const highlightFourthVideoPath = "/assets/videos/highlight-fourth.mp4";
+export const exploreVideoPath = "/assets/videos/explore.mp4";
+export const frameVideoPath = "/assets/videos/frame.mp4";
+
+export const heroImgPath = "/assets/images/hero.jpeg";
+export const appleImgPath = "/assets/images/apple.svg";
+export const searchImgPath = "/assets/images/search.svg";
+export const bagImgPath = "/assets/images/bag.svg";
+export const watchImgPath = "/assets/images/watch.svg";
+export const rightImgPath = "/assets/images/right.svg";
+export const replayImgPath = "/assets/images/replay.svg";
+export const playImgPath = "/assets/images/play.svg";
+export const pauseImgPath = "/assets/images/pause.svg";
+
+export const yellowImgPath = "/assets/images/yellow.jpg";
+export const blueImgPath = "/assets/images/blue.jpg";
+export const whiteImgPath = "/assets/images/white.jpg";
+export const blackImgPath = "/assets/images/black.jpg";
+export const explore1ImgPath = "/assets/images/explore1.jpg";
+export const explore2ImgPath = "/assets/images/explore2.jpg";
+export const chipImgPath = "/assets/images/chip.jpeg";
+export const frameImgPath = "/assets/images/frame.png";
